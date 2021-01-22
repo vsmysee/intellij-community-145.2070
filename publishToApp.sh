b736374f8095ee473603a76de628dbc94b266052
codingbaby <vsmysee@gmail.com>
2021-01-22 22:22:23 +0800

    update

---
+++ publishToApp.sh
@@ -0,0 +1,3 @@
+cp -R "/Volumes/data/building/intellij-community-145.2070/out/dist.all.ce/plugins/oh-my-idea/lib" "/Applications/IntelliJ IDEA CE.app/Contents/plugins/oh-my-idea"
+cp -R "/Volumes/data/building/intellij-community-145.2070/out/dist.all.ce/plugins/oh-my-idea/lib" "/Applications/IntelliJ IDEA CE 2.app/Contents/plugins/oh-my-idea"
+cp -R "/Volumes/data/building/intellij-community-145.2070/out/dist.all.ce/plugins/oh-my-idea/lib" "/Applications/Android Studio.app/Contents/plugins/oh-my-idea"
